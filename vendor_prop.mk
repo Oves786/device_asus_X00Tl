db20cc36cb025be4a9e8b02818ae809fcdff26c7
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-11-21 09:36:40 +0700

    X00T: set ro.telephony.iwlan_operation_mode to legacy
    since radio has been uprev to 1.4
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -242,8 +242,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.radio.calls.on.ims=1
-
-
+    ro.telephony.iwlan_operation_mode=legacy
 
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
